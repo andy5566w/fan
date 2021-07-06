3720b43d59cbfe52f255540b51d9e760fc6ee899
andy tsai <andy5566w@gmail.com>
2021-07-06 15:15:35 +0800

    feat: 新增mopup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import msg from './message'
-import React from 'react'
-console.log(msg)
+const mop_up_dom = document.querySelectorAll('[data-mop-up]')
+let is_run = true
+console.log(mop_up_dom)
+mop_up_dom.forEach(dom => dom.classList.add('run'))
+setInterval(() => {
+  mop_up_dom.forEach(dom => dom.classList.toggle('run'))
+}, 2500)
